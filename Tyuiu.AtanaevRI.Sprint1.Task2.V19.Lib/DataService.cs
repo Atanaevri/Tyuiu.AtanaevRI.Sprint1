d49851e7c8c2b3e144ec43845466971271d979be
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-02 20:00:22 +0500

    Task2

---
+++ Tyuiu.AtanaevRI.Sprint1.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AtanaevRI.Sprint1.Task2.V19.Lib
+{
+    public class DataService : ISprint1Task2V19
+    {
+        public double ConvertInchToKm(int x)
+        {
+            double meters = x * 0.0254;
+            return Math.Round(meters, 3);
+
+            
+        }
+    }
+}
+
+
+
---
+++ Tyuiu.AtanaevRI.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AtanaevRI.Sprint1.Task2.V19.Lib;
+namespace Tyuiu.AtanaevRI.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(0.127, res);
+        }
+    }
+}

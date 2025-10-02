93d74c87d751a8d77f6d12425738f8ab85c18aa0
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-10-02 14:18:07 +0500

    Task1

---
+++ Tyuiu.AtanaevRI.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AtanaevRI.Sprint1.Task1.V1.Lib;
+
+
+namespace Tyuiu.AtanaevRI.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 12.0;
+            double y = 2.0;
+            double a = 4.0;
+            var res = ds.Calculate(a, x, y);
+            Assert.AreEqual(26, res);
+        }
+    }
+}
